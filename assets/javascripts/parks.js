b65372fa683555a668b2ad013769866b39b49898
Ryan Travitz <rtravitz@gmail.com>
2017-01-09 21:51:08 -0700

    Slight card color changes

--- assets/javascripts/parks.js
+++ assets/javascripts/parks.js
@@ -7,7 +7,7 @@ var onFail = function(err){
 var onGetSuccess = function(parks){
   $.each(parks, function(index, park) {
     $('#parks-list').append(
-      '<div class="col sm12 m4"><div class="card brown"><div class="card-content white-text"><span class="card-title">' + park.name +
+      '<div class="col sm12 m4"><div class="card brown"><div class="card-content orange-text text-lighten-5"><span class="card-title">' + park.name +
       '</span>' + '<p>' + park.description.substring(0, 300) + '</p></div>' +
       '<div class="card-action"><a href="' + park.url + '">Visit the Park</a>' +
       '</div></div></div></div>'
--- assets/javascripts/search.js
+++ assets/javascripts/search.js
@@ -24,7 +24,7 @@ var onGetSuccess = function(parks){
   if (parks.length > 0) {
     $.each(parks, function(index, park) {
       $('.page-data').append(
-        '<div class="col sm12 m4"><div class="card brown"><div class="card-content white-text"><span class="card-title">' + park.name +
+        '<div class="col sm12 m4"><div class="card brown"><div class="card-content orange-text text-lighten-5"><span class="card-title">' + park.name +
         '</span>' + '<p>' + park.description.substring(0, 300) + '</p></div>' +
         '<div class="card-action"><a href="' + park.url + '">Visit the Park</a>' +
         '</div></div></div></div>'
